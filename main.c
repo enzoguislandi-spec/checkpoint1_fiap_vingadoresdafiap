a03cfa670767a6ef68f46e32a627c088f34716d4
Camper-Lingo <jcamperlingo@gmail.com>
2026-03-20 09:46:37 -0300

    Recebi o código atualizado e resolvi o problema de fechamento de aspas na linha 8

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf(Integrantes do grupo:\n);
-    printf("Lucas Silva de Abreu\n")
+    printf("Integrantes do grupo:\n");
+    printf("Lucas Silva de Abreu\n");
+    printf("Nicolas Castana Nishi\n");
+    printf("Enzo Giovani Guislandi\n");
+    printf("guilherme augusto da silva reiche\n")
 
     return 0;
 }
