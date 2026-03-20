f3057aeb7444999be33f0b813d724bd0208decba
enzoguislandi-spec <enzoguislandi@gmail.com>
2026-03-20 09:34:28 -0300

    Tirei a letra  H do nome  Nicolas na linha 6 e adicionei meu nome na linha 7!

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 int main() {
     printf("Integrantes do grupo:\n");
     printf("Lucas Silva de Abreu\n");
-    printf("Nicholas Nishi\n");
+    printf("Nicolas Nishi\n");
+    printf("enz o Guislandi\n");
 
     return 0;
 }
