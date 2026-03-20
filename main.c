38ff93e2edc88aeed9d5ddba6a33479ba49553cf
reicheguilherme-spec <reicheguilherme@gmail.com>
2026-03-20 09:42:41 -0300

    Adicionei meu nome na lista, depois do joão já podemos rodar o código!

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main() {
     printf("Lucas Silva de Abreu\n");
     printf("Nicolas Castana Nishi\n");
     printf("Enzo Giovani Guislandi\n");
+    printf "guilherme augusto da silva reiche\n)
 
     return 0;
 }
