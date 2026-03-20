abde20e360387c5dbca0c35f45c5cb2759ba098d
reicheguilherme-spec <reicheguilherme@gmail.com>
2026-03-20 09:40:35 -0300

    Alterei os nomes do Enzo e do Nicolas, que não estavam completos!

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 int main() {
     printf("Integrantes do grupo:\n");
     printf("Lucas Silva de Abreu\n");
-    printf("Nicolas Nishi\n");
-    printf("Enzo Guislandi\n");
+    printf("Nicolas Castana Nishi\n");
+    printf("Enzo Giovani Guislandi\n");
 
     return 0;
 }
