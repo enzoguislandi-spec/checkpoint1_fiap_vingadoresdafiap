bbb943745337d24f019546764fe3ab2b2ba45059
reicheguilherme-spec <reicheguilherme@gmail.com>
2026-03-20 09:38:35 -0300

    Olá grupo, entrei no código e arrumei o nome Enzo na linha 7!

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main() {
     printf("Integrantes do grupo:\n");
     printf("Lucas Silva de Abreu\n");
     printf("Nicolas Nishi\n");
-    printf("enz o Guislandi\n");
+    printf("Enzo Guislandi\n");
 
     return 0;
 }
